baf7652907878860a12a96b10f78d33c2e8edde9
Stephen Adjignon <sadjignon015@gmail.com>
2025-07-10 10:41:08 +0000

    Query files method added

--- crates/quasar-drive/src/gdrive/gdrive.rs
+++ crates/quasar-drive/src/gdrive/gdrive.rs
@@ -5,7 +5,25 @@ use google_drive3::{
     DriveHub, FieldMask, hyper, hyper_rustls, hyper_util,
     yup_oauth2 as oauth2
 };
-use std::collections::HashMap;
+use crate::hyper_util::client::legacy::connect::HttpConnector;
+
+use std::{
+    collections::HashMap,
+    pin::Pin,
+    future::Future,
+    result::Result
+};
+
+
+
+// let scopes = vec![
+//     "https://www.googleapis.com/auth/drive/file",
+//     "https://www.googleapis.com/auth/drive",
+//     "https://www.googleapis.com/drive.metadata"
+// ];
+//
+
+type BoxFutureResult<'a, T> = Pin<Box<dyn Future<Output = Result<T, anyhow::Error> + Send + 'a>>>;
 
 
 type GDriveMetadata = HashMap<String, String>;
@@ -15,7 +33,6 @@ type GDrivePermissions = Vec<HashMap<String, String>>;
 struct GDrive {
     credentials: oauth2::ServiceAccountKey,
     drive_hub: DriveHub,
-    ud_scopes: Vec<&str>
 }
 
 
@@ -54,12 +71,7 @@ impl GDrive {
              hyper_util::rt::TokioExecutor::new()
         ).build(https);
 
-        let scopes = vec![
-            "https://www.googleapis.com/auth/drive.file",
-            "https://www.googleapis.com/auth/drive",
-            "https://www.googleapis.com/auth/drive.metadata"
-        ];
-        let auth = oauth2::ServiceAccountAuthenticator::builder(
+       let auth = oauth2::ServiceAccountAuthenticator::builder(
                 secret
         )
         .build()
@@ -69,23 +81,54 @@ impl GDrive {
 
         Self {
             credentials: path,
-            drive_hub: drive_hub,
-            ud_scopes: scopes
+            drive_hub: drive_hub
         }
     }
 
-    pub fn query_folder(self, folder_id: Optional<String>) -> GDriveFile {
-        // todo!("I am yet to implement this because of the relation b/n drive and file");
-        let folder_mime_type = "application/vnd.google-apps.document";
-        let query = format!("mimeType='{}'", folder_mime_type)
-        self.drive_hub.folder
+
+    pub async fn query(self, query: &str) -> Vec<File> {
+        // Takes care of querying files given a query parameter
+        let scopes: Vec<&str> = vec![
+            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/drive",
+            "https://www.googleapis.com/auth/drive.metadata"
+        ];
+        let mut files: Vec<File> = vec![];
+        let mut page_token: Option<String> = None;
+        loop {
+            let mut req = self.drive_hub.files().list()
+                .add_scopes(&scopes)
+                .param(
+                    "fields",
+                    "nextPageToken,files(id,name,permissions,parents,mimeType,createdTime,modifiedTime)"
+                )
+                .q("mimeType!='application/vnd.google-apps.folder'")
+                .page_size(100);
+
+            if let Some(ref token) = page_token {
+                req = req.page_token(token)
+            }
+
+            let (_, file_list) = req
+                .doit()
+                .await
+                .expect("Fecthing drive response failed!");
+
+            if let Some(file_list_content) = file_list.files {
+                files.extend_from_slice(file_list_content.as_slice());
+            }
+
+            match file_list.next_page_token {
+                Some(token) => {
+                    page_token = Some(token)
+                }
+                None => break,
+            }
+        }
+
+        files
     }
+
 }
 
 
-
-impl GDriveFile {
-    pub fn file_path(drive: GDrive, folder_id: String) -> Vec<String> {
-        todo!("Implement the file reconstruction path");
-    }
-}
